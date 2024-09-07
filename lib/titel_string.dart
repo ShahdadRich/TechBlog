41484b7a22f09d80298471cb55fd83ab8668a8df
ShahdadRich <skylla3784@gmail.com>
2024-09-07 17:33:25 +0330

    some thing change

---
+++ lib/titel_string.dart
@@ -0,0 +1,7 @@
+class TitleString {
+  static const String viewHottestBlog = "مشاهده داغ ترین نوشته ها";
+  static const String viewHottestPodcast = "مشاهده داغ ترین پادکست ها";
+  static const String recommendedBlog = "نوشته های مرتبط";
+  static const String submitArticle = "ارسال مقاله";
+  static const String submitPodcast = "ارسال پادکست";
+}
